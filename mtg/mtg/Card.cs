183ad6cbdec4d66372121edde495c2af0343314d
justbark <justbark89@gmail.com>
2015-09-09 23:17:42 -0500

    json.dll and added card database
    added card database and started using the json.dll

---
+++ mtg/mtg/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtg
+{
+    class Card
+    {
+        public string name;
+        public string manaCost;
+        public int cmc;
+        public string colors;
+        public string type;
+        public string superTypes;
+        public string types;
+        public string subTypes;
+        public string rarity;
+        public string text;
+        public string flavor;
+        public string artist;
+        public int nunmber;
+        public int power;
+        public int toughness;
+        public string layout;
+        public int multiverseID;
+        public string imageName;
+        public string ID;
+    }
+}
--- mtg/mtg/Program.cs
+++ mtg/mtg/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,35 @@ namespace mtg
     {
         static void Main(string[] args)
         {
+            //string json = @"{
+            //    'Email': 'james@example.com',
+            //    'Active': true,
+            //    'CreatedDate': '2013-01-20T00:00:00Z',
+            //    'Roles': [
+            //    'User',
+            //    'Admin'
+            //    ]
+            //    }";
+            //
+            //    Account account = JsonConvert.DeserializeObject<Account>(json);
+            //    Console.WriteLine(account.Email);
+            //    Console.ReadLine();
+                // james@example.com
+            try
+            {
+                using (StreamReader sr = new StreamReader("../../AllCards-x.json"))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
